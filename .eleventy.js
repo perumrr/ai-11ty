f976687489c9eba6289222c74203f7c44fa8bdb0
perumrr <74992461+perumrr@users.noreply.github.com>
2025-03-02 18:29:50 +0000

    passthru

--- .eleventy.js
+++ .eleventy.js
@@ -3,5 +3,5 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
-  eleventyConfig.addPassthroughCopy("js.json");
+  eleventyConfig.addPassthroughCopy("main.js");
 };
