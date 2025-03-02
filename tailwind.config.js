36f4ba62c870903d91c09404206303bb414d90ec
perumrr <74992461+perumrr@users.noreply.github.com>
2025-03-02 18:19:55 +0000

    configuring tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          darkBg: "#121212",
+          darkCard: "#1e1e1e",
+          darkText: "#e0e0e0",
+          primaryAccent: "#bb86fc",
+        },
+      },
+    },
+    plugins: [],
+  };
+  
